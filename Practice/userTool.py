e9008299ce115d1c769d74330e8b3dd9838ac0e5
Kenny <kce6@njit.edu>
2018-10-11 23:45:40 -0400

    Revert "Added checkInput Function and other small additions"
    This reverts commit 28ae7646fa5a8cd90aed9b294aa8b80853f6d1a2.

--- Practice/userTool.py
+++ Practice/userTool.py
@@ -1,6 +1,6 @@
 ################################################################################
-#                       THE SYSADMIN'S USER CREATION                           #
-#                           AND MODIFICATION TOOL                              #
+#                       THE SYSADMIN'S USER MODIFICATION                       #
+#                               AND CREATION TOOL                              #
 #                                                                              #
 #           This program was created to make adding, removing, or otherwise    #
 #        modifying users on a *nix based system.  This program is freely       #
@@ -11,21 +11,22 @@
 
 # Imports
 
-import csv
-import subprocess
+import os as linux
 import time
-import re
 
 # Functions
+
+
 # Main Menu
 def mainMenu():
     print("\nWelcome to the User Creation Service\n")
     print("Please select one of the following options: \n")
-    print("1.  Create a new user")
-    print("2.  Create a new administrator")
-    print("3.  Modify the group of an existing user")
-    print("4.  Remove a user")
-    print("5.  Exit the program")
+    print("1.  Create a new user with standard permissions")
+    print("2.  Create a new user with advanced permissions")
+    print("3.  Create a new administrator")
+    print("4.  Modify the group of an existing user")
+    print("5.  Remove a user")
+    print("6.  Exit the program")
 
     # try catch to ensure user is entering a number
     while True:
@@ -40,89 +41,55 @@ def mainMenu():
             break
 
     if choice == 1:
-        print("\nYou have chosen to create a new user with standard permissions")
-        time.sleep(1)
+        print("You have chosen to create a new user with standard permissions\n")
+        time.sleep(3)
         print("Loading new user creation interface...")
-        time.sleep(1)
-        newUser()
+        time.sleep(3)
+        newUserStandard()
 
     elif choice == 2:
-        print("\nYou have chosen to create a new administrator")
-        time.sleep(1)
-        print("Loading new administrator creation interface...")
-        time.sleep(1)
-        newAdmin()
+        print("You have chosen to create a new user with advanced permissions\n")
+        time.sleep(seconds=3)
+        print("Loading new user creation interface...")
+        time.sleep(seconds=3)
+        newUserAdvanved()
 
     elif choice == 3:
-        print("\nYou have chosen to modify the group of an existing user")
-        time.sleep(1)
-        print("Loading modify group interface...")
-        time.sleep(1)
-        modifyGroup()
+        print("You have chosen to create a new administrator\n")
+        time.sleep(3)
+        print("Loading new administrator creation interface...")
+        time.sleep(3)
+        newAdmin()
 
     elif choice == 4:
-        print("\nYou have chosen to remove a user")
-        time.sleep(1)
-        print("Loading remove user interface...")
-        time.sleep(1)
-        removeUser()
+        print("You have chosen to modify the group of an existing user\n")
+        time.sleep(3)
+        print("Loading modify group interface...")
+        time.sleep(3)
+        modifyGroup()
 
     elif choice == 5:
-        print("\nExiting program...")
-        time.sleep(1)
+        print("You have chosen to remove a user\n")
+        time.sleep(3)
+        print("Loading remove user interface...")
+        time.sleep(3)
+        removeUser()
+
+    elif choice == 6:
+        print("Exiting program...")
+        time.sleep(2)
         exit()
 
     else:
-        print("\nThat was not one of the options, please choose again")
-        time.sleep(1)
+        print("That was not one of the options, please choose again")
+        time.sleep(2)
         mainMenu()
 
-# Sanitize User Input
-def checkInput(prompt):
-    while True:
-        result = input(prompt).strip()
-        try:
-            if not result:
-                raise ValueError
-            if not re.match("^[ a-zA-Z]*$", result):
-                raise ValueError
-        except ValueError:
-            print("Numbers are not allowed and the field CAN NOT be blank!")
-        else:
-            return result
+def newUserStandard():
+    pass
 
-# New User
-def newUser():
-    # Full Name
-    print("Welcome to the New User Creation Interface\n")
-    time.sleep(1)
-    prompt = "First things first, what is the full name (First and Last) of the New User? > "
-    fullName = checkInput(prompt)
-    time.sleep(1)
-    print("\nGreat! The name for the new user is: " + fullName)
-    print("\n")
-
-    # User Name
-    time.sleep(1)
-    prompt = """Next, what is the username of the User?  (We recommend using
-first initial and full last name) > """
-    userName = checkInput(prompt)
-    time.sleep(1)
-    print("\nGreat! The username for the new user is: " + userName)
-    print("\n")
-
-    # Group
-    time.sleep(1)
-    prompt = """Next, what group would you like the user to be added to?
-(NOTE: The user will be created with a default group that is the same as their
-username.  If you would like that user to be added to an additional group,
-i.e. HR, Management, IT, etc., please specify it here) > """
-    userGroup = checkInput(prompt)
-    time.sleep(1)
-
-    # Check to see if the group specified exisits.  If it doesn't ask user if
-    # it should be created
-    
+def newUserAdvanved():
+    pass
 
 def newAdmin():
     pass
